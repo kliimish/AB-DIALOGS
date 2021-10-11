9b2dcb16d3e44b3e3c72fbbb8210a569325f96e4
KLMS <kaspars.klims@gmail.com>
2021-10-11 20:23:14 +0300

    Darbs ar JS datu tipiem un mainigajiem

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 `use strict`;
+
 let num = 1; 
 
 let string =`Hello`;
@@ -11,5 +12,35 @@ let array = [];
 
 let funkcija = function(){};
 
-const nemainigais;
+
+
+let output = document.querySelector(`#output`);
+ console.log(output)
+let num1 =5; 
+
+let num2 = 8;
+
+let sum;
+
+let car1 = `vaz`;
+let car2 = `mercedes`;
+
+let arr = [`red`, `green`, `blue`];
+
+output.innerHTML = ` <li> ${sum = num1 + num2} We are ${typeof sum} </li>
+<li> ${car1} & ${car2} We are ${typeof car1} </li>
+<li> ${arr[0]} <br> ${arr[1]} <br> ${arr[2]} <br> We are ${typeof arr} </li>`; 
+
+
+
+
+
+
+
+
+
+
+
+
+
 

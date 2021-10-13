b5845a1ae970aa46d3add025f2414183cb13bfd1
KLMS <kaspars.klims@gmail.com>
2021-10-13 19:31:09 +0300

    Class10

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 `use strict`;
 
+//-------------- Variables(mainīgie)
 let num = 1; 
 
 let string =`Hello`;
@@ -8,14 +9,14 @@ let boolean = true;
 
 let object ={};
 
-let array = [];
+let array1 = [];
 
 let funkcija = function(){};
 
 
 
-let output = document.querySelector(`#output`);
- console.log(output)
+let variables = document.querySelector(`#variables`);
+
 
  let num1 =5; 
 
@@ -28,32 +29,62 @@ let car2 = `mercedes`;
 
 let arr = [`red`, `green`, `blue`];
 
-output.innerHTML = ` <li> ${sum = num1 + num2} We are ${typeof sum} </li>
+variables.innerHTML = ` 
+<li> ${sum = num1 + num2} We are ${typeof sum} </li>
 <li> ${car1} & ${car2} We are ${typeof car1} </li>
-<li> ${arr[0]} <br> ${arr[1]} <br> ${arr[2]} <br> We are ${typeof arr} </li>`; 
+<li> ${arr[0]} <br> ${arr[1]} <br> ${arr[2]} <br> We are ${typeof arr} </li>
+`; 
 
 let johnDoe = `John Doe`;
 
 console.log(johnDoe);
 
 
-{
-    let x = 10;
-    {
-        console.log(`${x+x} viss darbojās, mainīgais definēts ārējā blokā, so it works`);
-    }
-}
+// {
+//     let x = 10;
+//     {
+//         console.log(`${x+x} viss darbojās, mainīgais definēts ārējā blokā, so it works`);
+//     }
+// }
 
-console.log(`${x+x} es nedarbojos, jo neatrodos pareizajā blokā!`);
-
-
+// console.log(`${x+x} es nedarbojos, jo neatrodos pareizajā blokā!`);
 
 
 
+// ---------------- ARRAYS (MASīvi)
+
+let array = document.querySelector(`#array`);
+console.log(array);
+
+const cars =[`volvo`,`bmw`,`audi`];
+
+console.log(cars)
+cars[0] = `Zaz`;
+
+array.innerHTML = `<li>${cars} </li>`;
 
 
+console.log(cars.length);
+
+// Masīva pēdējo elementu iegūst length - 1
+
+//push metode (pievieno masīvam beigās jauno vērtību)
+cars.push(`gaz`);
+console.log(cars);
 
 
+//-------------Masīvu metodes---------------------------
+
+//toString() pārveido simbolu rindā vienā garā (katram simbolam ir savs indekss);
 
 
+// noņemt pēdējo elementu ar pop() metodi.
+cars.pop();
+console.log(cars);
+
+//elementa bīdīšana shift() atbilstoši pārindeksē masīvu;
+
+let shiftedValue = cars.shift();
+console.log(cars);
+console.log(shiftedValue);
 
